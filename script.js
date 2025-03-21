d0717c65df816a6ce57ff05298d297ec9688a553
infofbnr <crazyjim0123@gmail.com>
2025-03-21 20:05:50 +0200

    f

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCHYnW3qaNo7oGKMPs9DFALdWXIeYv6ixY",
   authDomain: "gossip-38bf8.firebaseapp.com",
   projectId: "gossip-38bf8",
-  storageBucket: "gossip-38bf8.appspot.com",
+  storageBucket: "gossip-38bf8.firebasestorage.app",
   messagingSenderId: "224975261462",
   appId: "1:224975261462:web:f08fd243ec4a5c1a4a4a37",
   measurementId: "G-N7S9894R3N"
